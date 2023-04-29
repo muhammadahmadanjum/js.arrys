edbd82612230f83a00f3ab685897d87eb4423eb1
muhammadahmadanjum <ahmedanjum688@gmail.com>
2023-04-29 08:54:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,147 @@
+// Array method concat
+// const Array1 = [ 'ahmad', 'asad', 'azhar']
+// const Array2 = [ 'lahore', 'islamabad', 'karachi']
+// const Array3 = Array1.concat(Array2);
+// console.log(Array3);
+
+// const Array4 = [ 'saad', 'daad', 'hamza']
+// const array5 = [ 'lahore1', 'islamabad2']
+// const Array6 = Array4.concat(array5);
+// console.log(Array6);
+// method copywithin
+// const Array7 = ['asad', 'ahmad', 'zain', 'nida', 'yusra']
+// console.log(Array7.copyWithin(1, 3, 4));
+// let Array9 = ['mahi', 'snober', 'zain', 'asad','ahmad', 'azhar']
+// console.log(Array9.copyWithin(0, 1, 3));
+
+// const abc = ['ahmad', 'yusra', 'mahi', 'meri jan'];
+
+// const read = abc.push('wife');
+// console.log(read);
+// console.log(abc);
+
+// const kuh = ['i', 'love', 'you', 'mahi'];
+
+// const sds = kuh.push('wife');
+// console.log(sds);
+// console.log(kuh);
+
+// const kid = ['asad', 'ahmad', 'anti', 'mahi'];
+// console.log(kid.slice(2));
+// let abc = [];
+// console.log(!!abc)
+// const result = 3+4;
+// console.log(!result);
+// function addToNumber(number1, number2){
+// let result = number1 + number2;
+// return result;
+// }
+// const abc = addToNumber(12,23);
+// console.log(abc);
+
+// function ahmadToSa(number1,number2){
+// let result = number1 - number2; 
+// return result;
+// }
+// const ggy = ahmadToSa(21,33);
+// console.log(ggy);
+
+// function ahmadToOo(number1,number2){
+//     let result = number1 / number2; 
+//     return result;
+//     }
+//     const uui = ahmadToOo(87,33);
+//     console.log(uui);
+
+//     function ahmadToPp(number1,number2){
+//         let result = number1 * number2; 
+//         return result;
+//         }
+//         const lol = ahmadToPp(10,2);
+//         console.log(lol);
+
+//         function ahmadToIi(number1,number2){
+//             let result = number1 % number2; 
+//             return result;
+//             }
+//             const ppp = ahmadToIi(54,33);
+//             console.log(ppp);
+
+// java script is object 
+//  key pair value is (key: value)
+// generic function
+// const operatorAdd = () =>{
+//     try{
+//         console.log(a);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
+// operatorAdd();
+//  const operatorSdd = (number1, number2, opertor) =>{
+//      const opertors = {
+//          '+': number1 + number2,
+//          '-': number1 - number2,
+//          '*': number1 * number2,
+//          '%': number1 % number2,
+//        '/': number1 / number2
+//      };
+//     return opertors[opertor] ||'please enter your name';
+//  };
+//  let abc = operatorSdd(34,43,'^');
+//  console.log(abc);
+    // for loop function
+    // for(let x = 10; x>=1; x--){
+    //     console.log(x+"<br>");
+    // }
+    // for(let x = 1; x<=10; x++){
+    //     console.log(x + '\n')
+    // }
+//     const calculator = (addTwoNumber, sabTwoNumber) =>{
+//         try {
+//             for(let x = 1; x<=sabTwoNumber; x++){
+//                 console.log(`${addTwoNumber} * ${x} = ${sabTwoNumber}`);
+//             }
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// let abc = calculator(2,10);
+// console.log(abc);
+
+//  const haevyCalculator = (addToNumber, sabNumber) =>{
+//      try{
+//          for(let x = 1; x<=sabNumber; x++){
+//              console.log(`${addToNumber} * ${x} * ${sabNumber}`)
+//          }
+//      } catch (error){
+//          console.log(error.message)
+//      }
+//  }
+//  let harryUp = haevyCalculator(4,10);
+//  console.log(harryUp);
+
+
+// const parentFunction =(x) => {
+//     const getResult = x;
+//     return getResult;
+// }
+// const childFunction =() => {
+//     const abc = 1;
+//     return abc;
+// }
+// const daad =parentFunction(childFunction);
+// console.log(daad);
+
+
+
+
+
+
+
+
+
+
+
+
+
